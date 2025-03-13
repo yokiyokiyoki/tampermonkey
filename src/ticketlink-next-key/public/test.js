f6d052c0391e2588c7293597a0ead7979947c8d7
李友杰 <liyoujie@cicc.com.cn>
2025-03-13 13:20:07 +0800

    更新文档

---
+++ src/ticketlink-next-key/public/test.js
@@ -0,0 +1,45 @@
+// 在页面加载前运行
+(function() {
+    // 保存原始console方法
+    const originalConsole = {
+      log: console.log,
+      clear: console.clear,
+      debug: console.debug,
+      info: console.info,
+      warn: console.warn,
+      error: console.error
+    };
+    
+    // 保存原始alert方法
+    const originalAlert = window.alert;
+    
+    // 防止控制台检测
+    Object.defineProperty(window, 'console', {
+      get: function() {
+        return originalConsole;
+      },
+      set: function() {
+        return originalConsole;
+      },
+      configurable: false
+    });
+    
+    // 覆盖alert以防止弹出韩文错误信息
+    window.alert = function(msg) {
+      if(msg) {
+        console.log("防调试警告已拦截",msg);
+        return;
+      }
+      return originalAlert.apply(this, arguments);
+    };
+    
+    // 禁用各种事件监听器
+    const originalAddEventListener = EventTarget.prototype.addEventListener;
+    EventTarget.prototype.addEventListener = function(type, listener, options) {
+      if(type === "devtoolschange" || type === "resize" || type === "error") {
+        console.log("拦截了事件监听:", type);
+        return;
+      }
+      return originalAddEventListener.call(this, type, listener, options);
+    };
+  })();
--- src/ticketlink-next-key/readme.md
+++ src/ticketlink-next-key/readme.md
@@ -1,3 +1,4 @@
 由于ticketlink买票页面有打开控制台反调试代码，先把下面js脚本用whistle给代理了，这样就可以打开
 
-$https://www.ticketlink.co.kr/global/zh/reserve/plan/schedule/548649873?evfw=sGriNUQaFZAmTWHM reqHeaders://(request.if-modified-since=) reqHeaders://(request.if-none-match=) resBody://{ticketlink} statusCode://200
+$https://www.ticketlink.co.kr/global/zh/reserve/plan/schedule/2110305545 jsPrepend://{ticketlink}
+
